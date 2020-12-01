5c83c7e59891a274e998f2a8356940ceda2382db
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2020-12-01 22:18:09 +0530

    validating answer

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ var userName=readlineSync.question(chalk.bgCyan("What's your Name? "));
 console.log(chalk.bgYellow("Welcome " + userName + " to Do You Know Charan?"));
 
 console.log("---------------");
+
+
+function load(question,answer){
+    var userAnswer=readlineSync.question(question);
+    if(userAnswer.toUpperCase() === answer.toUpperCase()){
+      console.log(chalk.cyanBright("You are Right?"));
+      score=score+1;
+    }else{
+      console.log(chalk.cyanBright("You are Wrong"));
+      score=score-1;
+    }
+    console.log(chalk.yellowBright("Your score is " + score));
+    console.log("---------------");
+  }
